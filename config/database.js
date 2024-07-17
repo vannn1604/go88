4d7f4a0b8c6ef4618ef2dabc78b471f2a6edee1f
unknown <van.nn1604@gmail.com>
2024-07-17 16:23:29 +0700

    change db

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
 module.exports = {
 	// 'url': 'mongodb://127.0.0.1:27017',
-	'url': 'mongodb+srv://vannn1604:ZZyDyOqWXE6XrELD@go88.wwabzno.mongodb.net/?retryWrites=true&w=majority&appName=GO88',
+	// 'url': 'mongodb+srv://vannn1604:ZZyDyOqWXE6XrELD@go88.wwabzno.mongodb.net/?retryWrites=true&w=majority&appName=GO88',
+	'url': 'mongodb+srv://vannn1604:ZZyDyOqWXE6XrELD@clustergo88.uznvg3n.mongodb.net/?retryWrites=true&w=majority&appName=ClusterGo88',
 	'options': {
 		// 'dbName': 'CLUB3333', // red
 		/*
