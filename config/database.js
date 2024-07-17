4b987a4925cf0a29016ae0fc5e732fd13482daf4
unknown <van.nn1604@gmail.com>
2024-07-17 10:29:32 +0700

    config to mongo atlas host

--- config/database.js
+++ config/database.js
@@ -1,7 +1,8 @@
 module.exports = {
-	'url': 'mongodb://127.0.0.1:27017',
+	// 'url': 'mongodb://127.0.0.1:27017',
+	'url': 'mongodb+srv://vannn1604:ZZyDyOqWXE6XrELD@go88.wwabzno.mongodb.net/?retryWrites=true&w=majority&appName=GO88',
 	'options': {
-		'dbName': 'CLUB3333', // red
+		// 'dbName': 'CLUB3333', // red
 		/*
 		14.163.27.99
 		vannn1604
