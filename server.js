6c7c39fa303bcabf6d9b78aecc22e36aa7f818d0
unknown <van.nn1604@gmail.com>
2024-07-18 15:34:27 +0700

    change port to 8000

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(cors({
     origin: '*',
     optionsSuccessStatus: 200
 }));
-let port       = process.env.PORT || 80;
+let port       = 8000;
 let expressWs  = require('express-ws')(app);
 let bodyParser = require('body-parser');
 var morgan = require('morgan');
