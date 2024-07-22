23e022d90ae862edc60f641fede56835179c68e9
Văn <43088114+vannn1604@users.noreply.github.com>
2024-07-22 19:48:52 +0700

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(cors({
     origin: '*',
     optionsSuccessStatus: 200
 }));
-let port       = 8000;
+let port       = 80;
 let expressWs  = require('express-ws')(app);
 let bodyParser = require('body-parser');
 var morgan = require('morgan');
