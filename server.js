c8296b73839d8aae1e973b10e5cc99b7341c9405
Văn <43088114+vannn1604@users.noreply.github.com>
2024-07-26 17:51:26 +0700

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(cors({
     optionsSuccessStatus: 200
 }));
 let port       = 80;
-let expressWs  = require('express-ws')(app, options={"wsOptions": {"url": "3.1.202.136"}});
+let expressWs  = require('express-ws')(app, options={"wsOptions": {"socket": new WebSocket('ws://3.1.202.136')}});
 let bodyParser = require('body-parser');
 var morgan = require('morgan');
 // Setting & Connect to the Database
